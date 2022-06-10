97cddb5db1ff41d823794b44de6fc8b72ee817e1
Braskann <Lunerka@gmail.com>
2022-06-10 09:18:12 +0300

    begin

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// const App = {
+//     data() {
+//         return {
+//             headerCompu: false,
+//         }
+//     },
+//     methods: {
+//         burgerMenu() {
+//             alert('рудд')
+//         }
+//     },
+//     computed: {
+//         screenWidth() {
+//             return window.screen.width
+//         },
+//         headerMobile() {
+//             if (this.screenWidth >= 1000) {
+//                 return false
+//             } else {
+//                 return true
+//             }
+//         },
+//         headerComputer() {
+//             if (this.screenWidth < 1000) {
+//                 return false
+//             } else {
+//                 return true
+//             }
+//         }
+//     }
+// }
+
+// const app = Vue.createApp(App);
+// app.mount('#burger');

e7319c95b2e89b494c5dc113cc492503de344df7
Braskann <Lunerka@gmail.com>
2022-06-10 12:59:56 +0300

    stradau

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
-// const App = {
-//     data() {
-//         return {
-//             headerCompu: false,
-//         }
-//     },
-//     methods: {
-//         burgerMenu() {
-//             alert('рудд')
-//         }
-//     },
-//     computed: {
-//         screenWidth() {
-//             return window.screen.width
-//         },
-//         headerMobile() {
-//             if (this.screenWidth >= 1000) {
-//                 return false
-//             } else {
-//                 return true
-//             }
-//         },
-//         headerComputer() {
-//             if (this.screenWidth < 1000) {
-//                 return false
-//             } else {
-//                 return true
-//             }
-//         }
-//     }
-// }
+const App = {
+    data() {
+        return {
+            headerCompu: false,
+        }
+    },
+    methods: {
+        openBurger(){
+            const burger = document.querySelector('.header__burger')
+            burger.classList.toggle('open')
+            const menu = document.querySelector('.header__nav')
+            if (burger.classList.contains('open')) {
+                menu.style.opacity = '1'
+                menu.style.height = '100%'
+            } else {
+                menu.style.opacity = '0'
+                menu.style.height = '0'
+            }
+        }
+    }
+}
 
-// const app = Vue.createApp(App);
-// app.mount('#burger');
+const app = Vue.createApp(App);
+app.mount('#burger');

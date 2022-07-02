41f583a311dbbebd632e77d616996dc52bf873d2
Braskann <Lunerka@gmail.com>
2022-07-02 22:59:48 +0300

    1111

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const app = Vue.createApp({
     data() {
         return {
@@ -10,10 +11,8 @@ const app = Vue.createApp({
             burger.classList.toggle('open')
             const menu = document.querySelector('.header__nav')
             if (burger.classList.contains('open')) {
-                // menu.style.opacity = '1'
                 menu.style.display = 'flex'
             } else {
-                // menu.style.opacity = '0'
                 menu.style.display = 'none'
             }
         }
